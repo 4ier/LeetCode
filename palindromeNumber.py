c880b459fd79d478b47e4c5ff2e9f66b77015975
Fourier <yang.fourier@gmail.com>
2018-02-06 18:52:37 +0800

    fix  problem when the input is times of 10

--- palindromeNumber.py
+++ palindromeNumber.py
@@ -4,11 +4,24 @@ class Solution:
         :type x: int
         :rtype: bool
         """
-        if x < 0:
+        if x < 0 or (x != 0 and x % 10 == 0):
             return False
         tmp = 0
         while x > tmp:
             tmp = 10 * tmp + (x % 10)
+            # print("tmp" + str(tmp))
             x = x / 10
+            # print("x" + str(x))
         return (x == tmp or x == tmp / 10)
 
+
+def main():
+    sln = Solution()
+    # print(sln.isPalindrome(0))
+    print(sln.isPalindrome(10))
+    # print(sln.isPalindrome(9876789))
+    # print(sln.isPalindrome(98766))
+
+
+if __name__ == '__main__':
+    main()
